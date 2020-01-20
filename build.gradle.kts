1dd1b91c58d0c22cf56ec765dc647f431a5c8bd5
Hugo Gu <hugogu@outlook.com>
2020-01-20 16:01:51 +0800

    Add gradle publish plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "1.3.61"
+    `maven-publish`
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "io.hugogu"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -12,6 +13,37 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("io.gitlab.arturbosch.detekt:detekt-api:1.4.0")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("DetektSmells") {
+            from(components["java"])
+            artifactId = "detekt-smells"
+            pom {
+                description.set("Static code analysis extension for Detekt")
+                name.set("detekt-smells")
+                url.set("https://github.com/hugogu/detekt-smells")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Hugo Gu")
+                        name.set("Hugo Gu")
+                        email.set("hugogu@outlook.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/hugogu/detekt-smells")
+                }
+            }
+        }
+    }
 }
 
 tasks {
